87641ee74567bf527eb3626cd06fa733cfc4e657
Cai-Zhenhui <Zhenhui.Cai@foxmail.com>
2022-05-15 00:02:26 +0800

    完善Trace

--- miniEngine2D/Trace.h
+++ miniEngine2D/Trace.h
@@ -1,13 +1,17 @@
 ﻿#pragma once
 /*
 Feature list
-1、控制台窗口输出调试信息			√
-2、调试窗口、ViewDbg输出调试信息	√
-3、保存调试记录到文件				
-4、提供宏定义方式的调试接口		√
-5、提供函数方式的调试接口			
-6、提供从控制台窗口的输入			
-7、...
+1、控制台窗口输出调试信息
+2、调试窗口、ViewDbg输出调试信息
+3、提供宏定义方式的调试接口
+4、提供函数方式的调试接口
+5、提供详细\简约信息的调试接口
+6、...
+
+Todo list
+1、保存调试记录到文件
+2、提供从控制台窗口的输入
+3、...
 */
 
 #include <Windows.h>
@@ -25,12 +29,13 @@ Feature list
 int __cdecl DebugPrintlnA(char const* const _Format, ...);
 int __cdecl DebugPrintlnW(wchar_t const* const _Format, ...);
 
-#define MACRO_LogA(_Format, ...) DebugPrintlnA("[Line:%d Function:%s File:%s]" _Format##"\n", __LINE__, __FUNCSIG__, __FILE__, __VA_ARGS__)
-#define MACRO_LogW(_Format, ...) DebugPrintlnW(L"[Line:%d Function:%s File:%s]" _Format##"\n", __LINE__, MINI_TEXT(__FUNCSIG__), MINI_TEXT(__FILE__), __VA_ARGS__)
+#define MACRO_LogA(_Format, ...) DebugPrintlnA("[Line:%d Function:%s File:%s]" _Format, __LINE__, __FUNCSIG__, __FILE__, __VA_ARGS__)
+#define MACRO_LogW(_Format, ...) DebugPrintlnW(L"[Line:%d Function:%s File:%s]" _Format, __LINE__, MINI_TEXT(__FUNCSIG__), MINI_TEXT(__FILE__), __VA_ARGS__)
 
-int __cdecl FunctionDebugPrintlnA(char const* const _Format, char const* const _Info, va_list _ArgList);
-int __cdecl FunctionDebugPrintlnW(wchar_t const* const _Format, wchar_t const* const _Info, va_list _ArgList);
 int __cdecl FunctionDebugPrintlnA(char const* const _Format, va_list _ArgList);
+int __cdecl FunctionDebugPrintlnW(wchar_t const* const _Format,va_list _ArgList);
+int __cdecl FunctionDebugPrintlnDetailA(char const* const _Format, va_list _ArgList);
+int __cdecl FunctionDebugPrintlnDetailW(wchar_t const* const _Format, va_list _ArgList);
 
 #ifdef UNICODE
 #define MACRO_Log MACRO_LogW
@@ -39,4 +44,8 @@ int __cdecl FunctionDebugPrintlnA(char const* const _Format, va_list _ArgList);
 #define MACRO_Log MACRO_LogA
 #define FunctionDebugPrintln FunctionDebugPrintlnA
 #endif
+
+void exportLogToFile(const std::string logFilePath);
+
+//Test
 void test();
